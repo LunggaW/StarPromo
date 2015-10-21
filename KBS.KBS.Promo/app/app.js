18b4a2e18d7059402f02345d50ba2402f3355833
LUNGGAW <LUNGGAW@GMAIL.COM>
2015-10-21 13:58:18 +0700

    Slide Box

---
+++ KBS.KBS.Promo/app/app.js
@@ -0,0 +1,66 @@
+﻿angular.module('ionicApp', ['ionic', 'ionicApp.controllers'])
+
+//var app = angular.module('ionicApp', ['ionic', 'angularMoment'])
+
+//var onDeviceReady = function () {  
+//    console.log("Application start");
+//    angular.bootstrap(document.body, ['ionicApp']);
+//}
+
+.controller("MainCtrl", function () {
+    console.log("Main Controller says: Hello World");
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    //.state('main', {
+    //    url: "/main",
+    //    templateUrl: "app/templates/main.html",
+    //    controller: 'MainCtrl'
+    //})
+
+    //.state('page2', {
+    //    url: "/page2",
+    //    templateUrl: "app/templates/page2.html",
+    //})
+
+
+
+
+
+
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "app/templates/menu.html",
+        controller: 'AppCtrl'
+    })
+
+	.state('app.playlists',
+    {
+        url: "/playlists",
+        views: {
+            'menuContent': {
+                templateUrl: "app/templates/playlists.html",
+                controller: 'PlaylistsCtrl'
+            }
+        }
+    })
+
+    .state('app.slidebox',
+    {
+        url: "/slidebox",
+        views: {
+            'menuContent': {
+                templateUrl: "app/templates/SlideBox.html",
+                controller: 'SlideBoxCtrl'
+            }
+        }
+    })
+
+
+    // if none of the above states are matched, use this as the fallback
+    //$urlRouterProvider.otherwise('/main');
+    $urlRouterProvider.otherwise('/app/playlists');
+});
---
+++ KBS.KBS.Promo/app/controllers/playlists.js
@@ -0,0 +1,47 @@
+﻿angular.module('ionicApp.controllers', [])
+
+.controller('AppCtrl', function ($scope)
+{
+})
+
+.controller('PlaylistsCtrl', function ($scope)
+{
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+})
+
+.controller('PlaylistCtrl', function ($scope, $stateParams)
+{
+})
+
+.controller('SlideBoxCtrl', function ($scope, $ionicSlideBoxDelegate)
+{
+    $scope.items = friends;
+
+    $scope.slideHasChanged = function ()
+    {
+        //$scope.items.push({ name: 'John', age: 25, gender: 'boy' });
+        $ionicSlideBoxDelegate.update();
+    };
+
+    $scope.showImages = function(index)
+    {
+        $scope.activeSlide = index;
+        $scope.showModal('templates/promo-popover.html');
+    }
+});
+
+
+var friends = [
+  { name: 'John', age: 25, gender: 'boy', image:"images/908.png"},
+  { name: 'Jessie', age: 30, gender: 'girl' },
+  { name: 'Johanna', age: 28, gender: 'girl' },
+  { name: 'Joy', age: 15, gender: 'girl' },
+  { name: 'Mary', age: 28, gender: 'girl' }
+];
